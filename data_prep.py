bc01176acb26a1ba0e75596d3707b40c8ab89612
Gayane Taturyan <43474745+taturyan@users.noreply.github.com>
2024-01-31 02:34:18 +0100

    added data_prep

---
+++ data_prep.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+
+def get_lawschool_data():
+    
+    df = pd.read_csv('./data/lawschool.csv')
+    df = df.dropna()
+    y = df['ugpa'] #target: gpa in [0,4]
+    df = df.drop('ugpa', axis=1)
+    df['gender'] = df['gender'].map({'male': 1, 'female': 0})
+    df_bar = df['bar1']
+    df = df.drop('bar1', axis=1)
+    df['bar1'] = [int(grade == 'P') for grade in df_bar]
+    df['race'] = [int(race == 7.0) for race in df['race']] #setting S=1 for white, S=0 for non-white
+    S = df['race'] #sensitive attribute
+    df = df.drop('race', axis=1)
+    X = df.to_numpy() #features
+    
+    return X, S, y
+
+def get_communities_data():
+    
+    df = pd.read_csv('./data/communities.csv')
+    df = df.fillna(0)
+
+    sens_attrs = ['racepctblack', 'racePctWhite', 'racePctAsian', 'racePctHisp']
+    df['race'] = df[sens_attrs].idxmax(axis=1) #creating a new column based on ethnicity
+    df = df.drop(columns=sens_attrs)
+
+    df = df.drop(df[df['ViolentCrimesPerPop']==0].index)
+    y = df['ViolentCrimesPerPop'] #target
+    df = df.drop('ViolentCrimesPerPop', axis=1)
+
+    mapping = {'racePctWhite':1, 'racepctblack':0, 'racePctAsian':0, 'racePctHisp':0} 
+
+    S = df['race'].map(mapping) #sensitive attribute: S=1 for white, S=0 for non-white
+    df = df.drop('race', axis=1)
+
+    X = df.to_numpy() #features
+    
+    return X, S, y
+
+def get_frequencies(S):
+    p = []
+    for p_s in S.value_counts(1):
+        p.append(p_s)
+    return p 
